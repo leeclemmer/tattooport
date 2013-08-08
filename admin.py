85d9bc7d9d1c7571dfd4df7e4d285850ceea1761
leeclemmer <clemmerl@gmail.com>
2013-08-07 22:59:35 -0400

    Added Create Studio page.

--- admin.py
+++ admin.py
@@ -11,5 +11,15 @@ class AdminModels(BaseHandler):
 	def get(self):
 		self.render('admin_models.html', title='Data Models', active = 'models')
 
+class AdminStudio(BaseHandler):
+	def get(self, name):
+		info('name',name)
+		try:
+			self.render('admin_studio_%s.html' % (name), active_nav = 'studio')
+		except jinja2.TemplateNotFound:
+			self.redirect('/admin/models')
+
+
 app = webapp2.WSGIApplication([('/admin/?', AdminMain),
-							   ('/admin/models', AdminModels)], debug=True)
+							   ('/admin/models', AdminModels),
+							   ('/admin/models/studio/([0-9a-zA-Z]*?)', AdminStudio)], debug=True)
