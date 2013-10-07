cbc49301af618b71634fd1a22f80014fe81d668b
leeclemmer <clemmerl@gmail.com>
2013-10-07 13:51:33 -0400

    Finished Content admin part by which you can delete individual pictures from the popular streams.

--- admin_content.py
+++ admin_content.py
@@ -11,30 +11,82 @@ class AdminContent(BaseHandler):
 					active='content'
 					)
 
-class AdminPopularFrontpage(BaseHandler):
-	def get(self):
-		self.render('admin_popular_frontpage.html', 
-					title='Popular Frontpage',
-					active='content'
-					)
-
 class AdminPopularCities(BaseHandler):
 	def get(self):
+		# Get list of cities from Localities list
+		cities = KeyList.get_by_id('Localities').key_list
+
+		# Construct parent path strings
+		cities = sorted([city_key.pairs()[0][1] + '/' + \
+				  city_key.pairs()[1][1] + '/' + \
+				  city_key.pairs()[2][1] for city_key in cities])
+
+		# Remove non-featured cities
+		cities = [city for city in cities \
+				  if city.rsplit('/',1)[1] in FEATURED_CITIES]
+
 		self.render('admin_popular_cities.html', 
 					title='Pick a City',
-					active='content'
+					active='content',
+					cities=cities
 					)
 
-class AdminPopularCity(BaseHandler):
+class AdminPopularPage(BaseHandler):
+	def set_vars(self, city):		
+		if city != 'frontpage':
+			self.city = city.rsplit('/',1)[0].upper() + '/' + \
+			   	   city.rsplit('/',1)[1].title()
+		elif city == 'frontpage':
+			self.city = 'Frontpage'
+
+		# Get popular list ID
+		self.plid = helper.plid('%s' % (self.city,))
+
+		# Get popular list
+		self.pop_list = helper.get_pop_list(self.plid)
+		self.pop_list = sorted(self.pop_list, key=lambda x: x.created_time, reverse=True)
+
+		info('self.pop_list', self.pop_list)
+
 	def get(self, city):
-		self.render('admin_popular_city.html', 
-					title='Pick a City',
+		self.set_vars(city)
+
+		info('self.pop_list', self.pop_list)
+
+		self.render('admin_popular_page.html', 
+					title=self.city,
 					active='content',
-					city=city
+					city=self.city,
+					pop_list=self.pop_list				
 					)
+
+	def post(self, city):
+		self.set_vars(city)
+
+		# Get user ID to identify which pic to delete
+		userid = self.request.get('userid')
+
+		# Go through pop_list and remove user's pic
+		for pop_item in self.pop_list:
+			if pop_item.user.id == userid:
+				del self.pop_list[self.pop_list.index(pop_item)]
+
+		# Update list in DB and memcache
+		PopularList.put_pop_list(self.plid, self.pop_list)
+		memcache.set(self.plid, self.pop_list, time=60*60*2)
+
+		msg = "Item deleted."
+
+		self.render('admin_popular_page.html', 
+					title=self.city,
+					active='content',
+					city=self.city,
+					pop_list=self.pop_list,
+					msg=msg					
+					)
+
 
 app = webapp2.WSGIApplication([('/admin/content/?', AdminContent),
-							   ('/admin/content/popular/frontpage/?', AdminPopularFrontpage),
 							   ('/admin/content/popular/cities/?', AdminPopularCities),
-							   ('/admin/content/popular/(.*)/?', AdminPopularCity)
+							   ('/admin/content/popular/(.*)/?', AdminPopularPage)
 							  ], debug=True)
